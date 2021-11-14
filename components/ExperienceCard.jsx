3075b99aadaefd8b069aceb5e3469768b48e78f8
Shado2107 <pd39almeida@outlook.com>
2021-11-14 11:38:20 +0000

    initial commit

---
+++ components/ExperienceCard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import {
+	Card,
+	CardBody,
+	CardTitle,
+	CardSubtitle,
+	CardText,
+	CardHeader,
+	Col,
+} from "reactstrap";
+
+import Color from "color-thief-react";
+
+import { Fade } from "react-reveal";
+
+const ExperienceCard = ({ data }) => {
+	return (
+		<Col lg="4">
+			<Fade left duration={1000} distance="40px">
+				<Card
+					style={{ flex: 1 }}
+					className="shadow-lg--hover shadow border-0 text-center rounded"
+				>
+					<Color src={data.companylogo} format="hex">
+						{(color) => (
+							<CardHeader style={{ background: color.data }}>
+								<h5 className="text-white">{data.company}</h5>
+							</CardHeader>
+						)}
+					</Color>
+					<CardBody className="py-5">
+						
+						<CardTitle tag="h5">{data.role}</CardTitle>
+						<CardSubtitle>{data.date}</CardSubtitle>
+						<CardText className="description my-3 text-left">
+							{data.desc}
+							<ul>
+								{data.descBullets
+									? data.descBullets.map((desc) => {
+											return <li key={desc}>{desc}</li>;
+									  })
+									: null}
+							</ul>
+						</CardText>
+					</CardBody>
+				</Card>
+			</Fade>
+		</Col>
+	);
+};
+
+export default ExperienceCard;
